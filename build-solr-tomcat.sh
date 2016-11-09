f099abc74c6c31e5119a272b115bd265cbcf6a18
root <root@aws-mate-003.corporateict.domain>
2016-11-09 13:33:11 +0800

    removed obsolete tomcat setup

--- build-solr-tomcat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget -c http://apache.mirror.amaze.com.au/tomcat/tomcat-9/v9.0.0.M11/bin/apache-tomcat-9.0.0.M11.tar.gz
-tar xvf apache-tomcat-*.tar.gz
-mkdir solr-tomcat
-mv apache-tomcat*/* solr-tomcat/
-rmdir apache-tomcat*
-sed -i "s/ start / run /g" solr-tomcat/bin/startup.sh 
